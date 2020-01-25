e5c2b3b28c5a0f6dfb98db55c8d143dad787c5ba
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-25 21:57:12 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,11 +49,11 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-  length = src.length
   total = 0
-  length.times do |index|
-    if (src[index][0] % 2) == 0 && (src[index][1] % 2) == 0
-      total = total + src[index][0] + src[index][1]
+  row_index = 0
+  while row_index < src.length do
+    if (src[row_index][0] % 2) == 0 && (src[row_index][1] % 2) == 0
+      total = total + src[row_index][0] + src[row_index][1]
     end
   end
   total
